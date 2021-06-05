e2a4087da34071d0e3b21558145efd1c4a2d451c
Hossein <hosseinsalmani1373@gmail.com>
2021-06-05 21:02:49 +0430

    Strings

---
+++ main13.rb
@@ -0,0 +1,29 @@
+# متدهای رشته ای
+# puts "String".chr
+# puts "a".chr
+# puts "Ali".hex
+# a = "Ruby Language"
+# puts a.length
+# puts "   ".length
+# puts a.empty?
+# puts " ".empty?
+# puts "".empty?
+# puts a.count('a')
+
+# ===========================
+
+puts "Hello".insert(3, "ooo")
+x ="Salam MyFriend"
+puts x.insert(4, "bbbbb")
+x = "ruby"
+puts x.upcase
+y = "RUBY"
+puts y.downcase
+z = "Salam be to dooste azizam"
+puts "information".index('o')
+puts "ali".index('m')
+puts "ali".index('i')   # آی اینجا اندیس دومه چون از صفر میشماریم
+f = "asdsdf"
+puts f.clear
+puts f.insert(0, 'ad')
+puts f.clear
